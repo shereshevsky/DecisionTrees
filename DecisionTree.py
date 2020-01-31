fixed variance calculation

DecisionTree.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-
+from collections import defaultdict
 from split_functions import variance_split
 
 
@@ -18,40 +18,43 @@ class NpEncoder(json.JSONEncoder):
 
 class DecisionTree:
     def __init__(self, min_leaf, max_depth=3):
-        self.tree = None
         self.min_leaf = min_leaf
         self.max_depth = max_depth
+        self.feature_importance = defaultdict(int)
+        self.tree = None
+        self.train_x = None
+        self.train_y = None
+        self.feature_names = None
 
     def __repr__(self):
         return json.dumps(self.tree, cls=NpEncoder)
 
-    def learn_recursive(self, p_x, p_y, feature_names, level=0):
+    def learn_recursive(self, p_x, p_y, level=0):
         if (
                 np.var(p_x, axis=0).sum() == 0
                 or p_y.size <= self.min_leaf
                 or np.var(p_y) == 0
                 or level >= self.max_depth
         ):
-            return {"label": np.mean(p_y)}
+            return {"label": np.mean(p_y), "cnt": p_y.size}
 
-        for (split_value, best_var_reduction), best_feature_index in sorted(
-                [(variance_split(p_x[:, i], p_y), i) for i in range(p_x.shape[1])],
-                key=lambda _x: _x[0][1],
-                reverse=True):
+        # generate list of best variance reduction for each feature and find the best feature and position to split by
+        (split_value, best_var_reduction), best_feature_index = sorted(
+            [(variance_split(p_x[:, i], p_y, self.min_leaf), i) for i in range(p_x.shape[1])],
+            key=lambda _x: _x[0][1],
+            reverse=True)[0]
 
-            if not best_var_reduction:
-                return {"label": np.mean(p_y)}
+        # no good variance reduction options found -> return average
+        if not best_var_reduction:
+            return {"label": np.mean(p_y), "cnt": p_y.size}
 
-            l_mask = p_x[:, best_feature_index] < split_value
-            if len(p_x[l_mask]) < self.min_leaf or len(p_x[~l_mask]) < self.min_leaf:
-                continue
+        l_mask = p_x[:, best_feature_index] < split_value
 
-        curr_feature = feature_names[best_feature_index]
-        del feature_names[best_feature_index]
-        p_x = np.delete(p_x, best_feature_index, 1)
+        curr_feature = self.feature_names[best_feature_index]
 
-        l_node = self.learn_recursive(p_x[l_mask], p_y[l_mask], feature_names, level + 1)
-        be_node = self.learn_recursive(p_x[~l_mask], p_y[~l_mask], feature_names, level + 1)
+        self.feature_importance[curr_feature] += best_var_reduction
+        l_node = self.learn_recursive(p_x[l_mask], p_y[l_mask], level + 1)
+        be_node = self.learn_recursive(p_x[~l_mask], p_y[~l_mask], level + 1)
 
         return {
             "split_feature": curr_feature,
@@ -63,8 +66,8 @@ class DecisionTree:
     def fit(self, p_x, p_y):
         self.train_x = p_x
         self.train_y = p_y
-        feature_names = list(range(p_x.shape[1]))
-        self.tree = self.learn_recursive(p_x, p_y, feature_names)
+        self.feature_names = list(range(p_x.shape[1]))
+        self.tree = self.learn_recursive(p_x, p_y)
 
     def mse(self):
         predicted = self.predict(self.train_x)

split_functions.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 
-def variance_split(p_x, p_y):
+def variance_split(p_x: np.array, p_y: np.array, min_leaf: int):
+    """
+    Calculate variance for all the possible split positions
+    :param p_x: np.array of feature values
+    :param p_y: np.array of target values
+    :param min_leaf: minimal number values on leaf to avoid invalid splits
+    :return:
+    """
     # create 2d array of single feature and target values and sort by feature values.
     # 0 - feature, 1 - target
     arr = np.array(sorted(np.vstack([p_x, p_y]).T, key=lambda v: v[0])).T
@@ -10,10 +17,13 @@ def variance_split(p_x, p_y):
     # generate variance for all possible feature splits
     all_vars = []
     for i in range(p_y.size):
-        if i == 0 or arr[0][i] == arr[0][i - 1]:
+        # don't split < min_leaf size, don't split if the next value == current value
+        if i < min_leaf or i > (p_y.size - min_leaf) or arr[0][i] == arr[0][i-1]:
             all_vars.append(1000)
         else:
-            all_vars.append(np.var(arr[1][:i]) / i + np.var(arr[1][i:]) / (p_y.size - i))
+            all_vars.append(
+                (np.var(arr[1][:i]) / i + np.var(arr[1][i:]) / (p_y.size - i)) / p_y.size
+            )
 
     index_of_lowest_var = np.where(np.array(all_vars) == np.min(np.array(all_vars)))[0][0]
 

tests/test_split_var.py
@@ -9,13 +9,15 @@ p_y = pickle.load(open("tests/p_y0.pkl", "rb"))
 results = [(variance_split(p_x[:, i], p_y), i) for i in range(p_x.shape[1])]
 
 assert (
-    min(
-        [
-            np.var(p_y[:i]) / i + np.var(p_y[i:]) / (p_y.size - i)
-            for i in range(p_y.size)
-            if i > 0
-        ]
-    )
-    == 1.098721280759848
+        min(
+            [
+                np.var(p_y[:i]) / i + np.var(p_y[i:]) / (p_y.size - i)
+                for i in range(p_y.size)
+                if i > 0
+            ]
+        )
+        == 1.098721280759848
 )
-assert all([i[0][0] == -1 for i in results])
+assert results == [((-1, 0), 0), ((-1, 0), 1), ((0.74, 0.05316685548190847), 2), ((-1, 0), 3), ((-1, 0), 4),
+                   ((8.725, 0.05316685548190825), 5), ((-1, 0), 6), ((1.3459, 0.11030799831056826), 7), ((-1, 0), 8),
+                   ((-1, 0), 9), ((-1, 0), 10), ((-1, 0), 11), ((3.01, 0.05316685548190869), 12)]

tests/test_tree.py
@@ -69,14 +69,26 @@ from DecisionTree import DecisionTree
 #
 # print(dt.mse())
 
-from sklearn.datasets import load_boston
+### cricket
 
+y = np.array([  1,    0,      0,      1,      0,      1,      1,      0,      1])
+X = np.array([
+                [1,9],[1,8], [0,8],   [1,8],  [0,9],  [1,9],  [1,8],  [0,9],  [1,8]])
+
+dt = DecisionTree(2, 1)
+dt.fit(X, y)
+print(dt)
+
+###  Boston
+from sklearn.datasets import load_boston
+from sklearn.preprocessing.data import normalize
 boston = load_boston()
 boston_y = boston.target
 boston_X = boston.data
 
-dt = DecisionTree(2, 10)
-dt.fit(boston_X, boston_y)
-
+dt = DecisionTree(5, 10)
+dt.fit(normalize(boston_X,  axis=0), boston_y)
+print(dt)
+print(dt.feature_importance)
 print(dt.mse())
 